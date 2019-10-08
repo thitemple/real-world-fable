c5f9a9e7ae1222aedbfbccc1a7a61f4ba215c2d4
Thiago Temple <thiagot@surveymonkey.com>
2019-10-07 21:00:57 -0400

    Simplified webpack

--- webpack.config.js
+++ webpack.config.js
@@ -1,72 +1,20 @@
-const path = require("path");
-const webpack = require("webpack");
-const HtmlWebpackPlugin = require("html-webpack-plugin");
+var path = require("path");
 
 module.exports = {
-  mode: "development",
-  devtool: "eval-source-map",
-  entry: {
-    app: path.join(__dirname, "./src/RealWorldFable.fsproj")
-  },
-  output: {
-    path: path.join(__dirname, "./public"),
-    filename: "bundle.js"
-  },
-  devServer: {
-    contentBase: "./public",
-    port: 8080,
-    publicPath: "/",
-    hot: true,
-    inline: true
-  },
-  module: {
-    rules: [
-      {
-        test: /\.fs(x|proj)?$/,
-        use: "fable-loader"
-      },
-      {
-        test: /\.js$/,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-          options: {
-            presets: [
-              [
-                "@babel/preset-env",
-                {
-                  targets: "> 0.25%, not dead",
-                  modules: false,
-                  useBuiltIns: "usage",
-                  corejs: 3
-                }
-              ]
-            ]
-          }
-        }
-      },
-      {
-        test: /\.(png|jpg|jpeg|gif|svg|woff|woff2|ttf|eot)(\?.*)?$/,
-        use: ["file-loader"]
-      }
-    ]
-  },
-  plugins: [
-    new webpack.HotModuleReplacementPlugin(),
-    new HtmlWebpackPlugin({
-      filename: "index.html",
-      template: path.join(__dirname, "./src/index.html")
-    })
-  ],
-  optimization: {
-    splitChunks: {
-      cacheGroups: {
-        commons: {
-          test: /node_modules/,
-          name: "vendors",
-          chunks: "all"
-        }
-      }
+    mode: "development",
+    entry: "./src/RealWorldFable.fsproj",
+    output: {
+        path: path.join(__dirname, "./public"),
+        filename: "bundle.js",
+    },
+    devServer: {
+        contentBase: "./public",
+        port: 8080,
+    },
+    module: {
+        rules: [{
+            test: /\.fs(x|proj)?$/,
+            use: "fable-loader"
+        }]
     }
-  }
-};
+}
